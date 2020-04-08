dc10d8c3e94d1868a48ade2efb17e3f12f3e58bb
Nico Yeager <nicoyeager@gmail.com>
2020-04-08 16:41:06 -0400

    lesson-04 created loginvc

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Nico Yeager on 4/8/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
