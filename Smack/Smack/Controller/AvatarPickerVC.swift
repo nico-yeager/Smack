e0d07eb90f0afecc55d0afb0e267124c0ed06057
Nico Yeager <nicoyeager@gmail.com>
2020-04-12 17:11:49 -0400

    partway through lesson-10, stop for now

---
+++ Smack/Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,30 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by Nico Yeager on 4/12/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Nico Yeager on 4/12/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+}
